9b3b4eb951285b472769a07c648d91a5c67ceb36
beinjamin <jauresbeinjamin@gmail.com>
2022-03-25 22:51:33 +0100

    Creation de la page dasbord

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageLoginComponent } from './pages/page-login/page-login.component';
 import { PageInscriptionComponent } from './page-inscription/page-inscription.component';
+import { PageDashboardComponent } from './page-dashboard/page-dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PageLoginComponent,
-    PageInscriptionComponent
+    PageInscriptionComponent,
+    PageDashboardComponent
   ],
   imports: [
     BrowserModule,
