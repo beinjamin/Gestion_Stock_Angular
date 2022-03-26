40355915496d4f153ea37d4d7dbfb878a8b0b7e3
beinjamin <jauresbeinjamin@gmail.com>
2022-03-26 22:41:42 +0100

    Gestion des routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageDashboardComponent } from './page-dashboard/page-dashboard.component';
+import { PageInscriptionComponent } from './page-inscription/page-inscription.component';
+import { PageLoginComponent } from './pages/page-login/page-login.component';
 
 const routes: Routes = [
   {
     path: 'login',
-
     component: PageLoginComponent
   },
   {
-    path: 'inscription'
-    component:
-  }
+    path: 'inscription',
+    component: PageInscriptionComponent
+  },
+  {
+    path:'',
+    component: PageDashboardComponent,
+  children: [
+    {
+      path:'articles',
+    }
+  ]
+}
 ];
 
 @NgModule({
