2f5eb2c200726dea1cbcdd197ea00348efa03528
beinjamin <jauresbeinjamin@gmail.com>
2022-03-27 23:28:01 +0100

    Gestion de l'object Menu sous forme de tableau

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -8,13 +8,29 @@ import { Menu } from './menu';
 })
 export class MenuComponent implements OnInit {
 
-  public menuProperties : Menu = {
-    id:'',
-    titre: '',
+  public menuProperties : Array<Menu> = [{
+    id:'1',
+    titre: 'Tableau de bord',
     icon: '',
     url: '',
-  }
+    sousMenu: [
+      {
+        id:'11',
+        titre: 'Vue ensemble',
+        icon: '',
+        url:''
+      },
 
+      {
+        id:'2',
+        titre: 'Statistique',
+        icon: '',
+        url:''
+      }
+
+    ]
+  }
+  ];
   constructor() { }
 
   ngOnInit(): void {
