7d27057a374c9976da80900911cecf698b26ca9d
beinjamin <jauresbeinjamin@gmail.com>
2022-03-30 19:56:20 +0100

    Gestion du constructeur pour navigate

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Menu } from './menu';
 
 @Component({
@@ -134,9 +135,14 @@ export class MenuComponent implements OnInit {
 
 
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
+    navigate(url:string){
+
+    }
 }
