bd51eda94c4aedf38df5e1236d4ead30075c6a31
beinjamin <jauresbeinjamin@gmail.com>
2022-04-20 07:52:43 +0100

    Reorganisation du code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PageDashboardComponent } from './page-dashboard/page-dashboard.component';
+import { PageDashboardComponent } from './pages/page-dashboard/page-dashboard.component';
 import { PageInscriptionComponent } from './page-inscription/page-inscription.component';
 import { PageStatistiquesComponent } from './page-statistiques/page-statistiques.component';
 import { PageLoginComponent } from './pages/page-login/page-login.component';
